f10a7e0704009601b3aa8150a83034e25ab4961e
todd <toddcookevt@gmail.com>
2017-11-15 22:30:18 -0500

    create class to create example repo

--- app/src/main/java/com/android/example/github/walkingTale/ExampleRepo.kt
+++ app/src/main/java/com/android/example/github/walkingTale/ExampleRepo.kt
@@ -6,11 +6,13 @@ import com.android.example.github.vo.Repo
  * 11/15/17.
  */
 class ExampleRepo() {
-    fun getRepo(): Repo {
-        return Repo(
-                1, "", "", "", Repo.Owner("", ""),
-                1, mutableListOf(), "", "", "", "",
-                "", 1.1, 1.1, ""
-        )
+    companion object {
+        fun getRepo(): Repo {
+            return Repo(
+                    1, "", "", "", Repo.Owner("", ""),
+                    1, mutableListOf(), "", "", "", "",
+                    "", 1.1, 1.1, ""
+            )
+        }
     }
 }
