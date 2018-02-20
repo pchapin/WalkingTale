6f107fa011367d962530a0b045917fb45aa8bc9d
todd <toddcookevt@gmail.com>
2018-02-20 08:37:32 -0500

    made s3 bucket publicly readable so glide can cache images

--- app/src/main/java/com/android/example/github/binding/FragmentBindingAdapters.java
+++ app/src/main/java/com/android/example/github/binding/FragmentBindingAdapters.java
@@ -19,20 +19,14 @@ package com.android.example.github.binding;
 import android.databinding.BindingAdapter;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
-import com.android.example.github.aws.S3UtilKt;
-import com.android.example.github.ui.common.CreateFileKt;
+import com.android.example.github.aws.ConstantsKt;
 import com.bumptech.glide.Glide;
 
-import java.io.File;
 import java.io.IOException;
 
 import javax.inject.Inject;
@@ -41,6 +35,7 @@ import javax.inject.Inject;
  * Binding adapters that work with a fragment instance.
  */
 public class FragmentBindingAdapters {
+    private static final String s3HostName = ConstantsKt.getS3HostName();
     private final Fragment fragment;
     private final String TAG = this.getClass().getSimpleName();
     private MediaPlayer mp = new MediaPlayer();
@@ -52,47 +47,7 @@ public class FragmentBindingAdapters {
 
     @BindingAdapter("imageUrl")
     public void bindImage(ImageView imageView, String url) {
-
-        Log.i(TAG, "image url: " + url);
-        // The url should never be null, but just in case
-        if (url == null) return;
-
-        if (new File(url).isFile()) {
-            // This is still needed to show images on the device before they are uploaded to s3
-            Glide.with(fragment).load(url).into(imageView);
-            return;
-        }
-
-        File file = null;
-        try {
-            file = CreateFileKt.createFile(fragment.getActivity());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        File finalFile = file;
-
-        S3UtilKt.getTransferUtility(fragment.getContext())
-                .download(url, file)
-                .setTransferListener(new TransferListener() {
-                    @Override
-                    public void onStateChanged(int id, TransferState state) {
-                        Log.i(TAG, "state " + state);
-                        if (state == TransferState.COMPLETED) {
-                            Glide.with(fragment).load(Uri.fromFile(finalFile)).into(imageView);
-                        }
-                    }
-
-                    @Override
-                    public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
-
-                    }
-
-                    @Override
-                    public void onError(int id, Exception ex) {
-                        Log.i(TAG, "error " + ex);
-
-                    }
-                });
+        Glide.with(fragment).load(s3HostName + url).into(imageView);
     }
 
     @BindingAdapter("audioUrl")
