385aa6886bb1ce7e8808cdcef1c0b7104c5b6669
todd <toddcookevt@gmail.com>
2018-04-13 08:03:11 -0400

    allow multiline text up to 480 chars in text input activity

--- app/src/main/java/com/talkingwhale/activities/TextInputActivity.kt
+++ app/src/main/java/com/talkingwhale/activities/TextInputActivity.kt
@@ -6,6 +6,8 @@ import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.talkingwhale.R
 import com.talkingwhale.databinding.ActivityTextInputBinding
@@ -21,23 +23,33 @@ class TextInputActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        postTextButton()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_text_input, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.action_create_text_post) {
+            postTextButton()
+            return true
+        }
+        return false
     }
 
     private fun postTextButton() {
-        button_text_post_submit.setOnClickListener {
-            val text = binding.postTextEditText.text.toString().trim()
-            if (text.isBlank()) {
-                Toast.makeText(this, "Text cannot be blank", Toast.LENGTH_SHORT).show()
-                post_text_edit_text.setText("")
-                return@setOnClickListener
-            }
-
-            val intent = Intent()
-            intent.putExtra(TEXT_KEY, text)
-            setResult(Activity.RESULT_OK, intent)
-            finish()
+        val text = binding.postTextEditText.text.toString().trim()
+        if (text.isBlank()) {
+            Toast.makeText(this, "Text cannot be blank", Toast.LENGTH_SHORT).show()
+            post_text_edit_text.setText("")
+            return
         }
+
+        val intent = Intent()
+        intent.putExtra(TEXT_KEY, text)
+        setResult(Activity.RESULT_OK, intent)
+        finish()
     }
 
     companion object {
