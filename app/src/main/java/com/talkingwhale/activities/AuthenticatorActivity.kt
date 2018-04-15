96ba03e4d1cdaa21040a8668c196a1bd36c85a3d
todd <toddcookevt@gmail.com>
2018-04-15 19:16:43 -0400

    change sign-in background image

--- app/src/main/java/com/talkingwhale/activities/AuthenticatorActivity.kt
+++ app/src/main/java/com/talkingwhale/activities/AuthenticatorActivity.kt
@@ -1,8 +1,9 @@
 package com.talkingwhale.activities
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
-
+import com.amazonaws.mobile.auth.ui.AuthUIConfiguration
 import com.amazonaws.mobile.auth.ui.SignInUI
 import com.amazonaws.mobile.client.AWSMobileClient
 import com.talkingwhale.R
@@ -13,10 +14,16 @@ class AuthenticatorActivity : Activity() {
         setContentView(R.layout.activity_authenticator)
 
         AWSMobileClient.getInstance().initialize(this) {
-            val signin = AWSMobileClient
-                    .getInstance()
-                    .getClient(this@AuthenticatorActivity, SignInUI::class.java) as SignInUI
-            signin.login(this@AuthenticatorActivity, MainActivity::class.java).execute()
+            val config = AuthUIConfiguration.Builder()
+                    .userPools(true)  // true? show the Email and Password UI
+                    //todo: change background image
+                    .logoResId(R.drawable.ic_directions_walk_black_24dp) // Change the logo
+                    .backgroundColor(Color.WHITE) // Change the backgroundColor
+                    .isBackgroundColorFullScreen(false) // Full screen backgroundColor the backgroundColor full screen
+                    .canCancel(true)
+                    .build()
+            val signinUI = AWSMobileClient.getInstance().getClient(this@AuthenticatorActivity, SignInUI::class.java) as SignInUI
+            signinUI.login(this@AuthenticatorActivity, MainActivity::class.java).authUIConfiguration(config).execute()
         }.execute()
     }
 }
