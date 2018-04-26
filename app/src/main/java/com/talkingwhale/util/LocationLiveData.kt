8d93a1f6243114e8f960c72466f94f2937fc6eab
todd <toddcookevt@gmail.com>
2018-04-26 10:27:17 -0400

    add preference to limit location updates

--- app/src/main/java/com/talkingwhale/util/LocationLiveData.kt
+++ app/src/main/java/com/talkingwhale/util/LocationLiveData.kt
@@ -4,20 +4,22 @@ import android.annotation.SuppressLint
 import android.arch.lifecycle.LiveData
 import android.content.Context
 import android.location.Location
+import android.support.v7.preference.PreferenceManager
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
+import com.talkingwhale.R
 
-class LocationLiveData(context: Context) : LiveData<Location>() {
+class LocationLiveData(val context: Context) : LiveData<Location>() {
 
     @SuppressLint("MissingPermission")
     private val fusedLocationClient = FusedLocationProviderClient(context)
-    private val locationRequest = LocationRequest().setInterval(10).setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
     private val locationCallback = object : LocationCallback() {
         override fun onLocationResult(p0: LocationResult?) {
-            if (p0 != null)
+            if (p0 != null) {
                 value = p0.lastLocation
+            }
         }
     }
 
@@ -31,6 +33,17 @@ class LocationLiveData(context: Context) : LiveData<Location>() {
 
     @SuppressLint("MissingPermission")
     private fun startLocationUpdates() {
+
+        val sp = PreferenceManager.getDefaultSharedPreferences(context)
+        val locationRequest = LocationRequest()
+        val isHighAccuracy = sp.getBoolean(context.resources.getString(R.string.pref_key_location_accuracy), false)
+
+        if (isHighAccuracy) {
+            locationRequest.setInterval(10).priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        } else {
+            locationRequest.setInterval(30).priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        }
+
         fusedLocationClient.requestLocationUpdates(
                 locationRequest,
                 locationCallback,
